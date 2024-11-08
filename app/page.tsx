73bdc177ed32435be31d752d39616500f66a0bdb
Nana Mohamadi <adilnana78@gmail.com>
2024-11-08 15:47:09 +0100

    Add a pricingCard component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,18 @@ import React from "react";
 import NavBar from "./components/navbar";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { pricingCards } from "./constants/landing-page";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import clsx from "clsx";
+import { Check } from "lucide-react";
+import Link from "next/link";
 
 const Home = () => {
   return (
@@ -26,8 +38,61 @@ const Home = () => {
           <Button className="bg-orange font-bold text-white px-4">
             Start For Free
           </Button>
+          <Image
+            src="/images/iphonecorinna.png"
+            width={400}
+            height={400}
+            alt="LOGO"
+            className="max-w-lg object-contain"
+          />
         </div>
       </section>
+      <section className="flex justify-center items-center flex-col gap-4 mt-10">
+        <h2 className="text-4xl text-center">Choose what fits you right</h2>
+        <p className="text-muted-foreground text-center max-w-lg">
+          Our straightforward pricing plans are tailored to meet your needs. If{" "}
+          {" you're"} not ready to commit you are can get started for free.
+        </p>
+      </section>
+      <div className="flex flex-wrap mt-6 justify-center gap-4">
+        {pricingCards.map((card) => (
+          <Card
+            key={card.title}
+            className={clsx("w-[300px] flex flex-col justify-between", {
+              "border-2 border-primary": card.title === "Unlimited",
+            })}
+          >
+            <CardHeader>
+              <CardTitle className="text-orange">{card.title}</CardTitle>
+              <CardDescription>
+                {pricingCards.find((c) => c.title === card.title)?.description}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <span className="text-4xl font-bold">{card.price}</span>
+              <span className="text-muted-foreground">
+                <span>/ month</span>
+              </span>
+            </CardContent>
+            <CardFooter className="flex flex-col items-start gap-4">
+              <div>
+                {card.features.map((feature) => (
+                  <div key={feature} className="flex gap-2">
+                    <Check />
+                    <p>{feature}</p>
+                  </div>
+                ))}
+              </div>
+              <Link
+                href={`/dashboard?plan=${card.title}`}
+                className="bg-[#f3d299] border-orange border-2 p-2 w-full text-center font-bold rounded-md"
+              >
+                Get Started
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </main>
   );
 };
