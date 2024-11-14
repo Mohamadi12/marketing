32fa631d6d32540af2db6cbf8cce6aa946f4978f
Nana Mohamadi <adilnana78@gmail.com>
2024-11-14 01:55:02 +0100

    Current Plan

--- app/components/settings/billing-setting.tsx
+++ app/components/settings/billing-setting.tsx
@@ -2,12 +2,18 @@ import { onGetSubscriptionPlan } from "@/app/actions/settings";
 import React from "react";
 import Section from "../section-label";
 import { Card, CardContent, CardDescription } from "@/components/ui/card";
-import { Plus } from "lucide-react";
+import { CheckCircle2, Plus } from "lucide-react";
+import { pricingCards } from "@/app/constants/landing-page";
 
 type Props = {};
 
 const BillingSetting = async (props: Props) => {
   const plan = await onGetSubscriptionPlan();
+  const planFeatures = pricingCards.find(
+    (card) => card.title.toUpperCase() === plan?.toUpperCase()
+  )?.features;
+  if (!planFeatures) return;
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-5 gap-10">
       <div className="lg:col-span-1">
@@ -31,13 +37,14 @@ const BillingSetting = async (props: Props) => {
       <div className="lg:col-span-2">
         <h3 className="text-xl font-semibold mb-2">Current Plan</h3>
         <p className="text-sm font-semibold">{plan}</p>
-        <p className="text-sm font-light">
-          {plan == "PRO"
-            ? "Start growing your business today"
-            : plan == "ULTIMATE"
-            ? "The ultimate growth plan that sets you up for success"
-            : `Perfect if you're just getting with Corinna AI`}
-        </p>
+        <div className="flex gap-2 flex-col mt-2">
+          {planFeatures.map((feature) => (
+            <div key={feature} className="flex gap-2">
+              <CheckCircle2 className="text-muted-foreground" />
+              <p className="text-muted-foreground">{feature}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
