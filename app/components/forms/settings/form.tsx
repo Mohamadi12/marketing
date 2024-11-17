08e43a9f02d4dc679d771c37ff43acf1f68101d0
Nana Mohamadi <adilnana78@gmail.com>
2024-11-18 00:31:58 +0100

    Bot customization options and improve performance in Nextjs by dynamically importing components

--- app/components/forms/settings/form.tsx
+++ app/components/forms/settings/form.tsx
@@ -4,6 +4,15 @@ import { useSettings } from "@/app/hooks/settings/use-settings";
 import { Separator } from "@/components/ui/separator";
 import { DomainUpdate } from "./domain-update";
 import CodeSnippet from "./code-snippet";
+import PremiumBadge from "@/public/icons/premium-badge";
+import EditChatbotIcon from "./edit-chatbot-icon";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+
+const WelcomeMessage = dynamic(
+  () => import("./greetings-message").then((props) => props.default),
+  { ssr: false }
+);
 
 type Props = {
   id: string;
@@ -26,12 +35,48 @@ const SettingsForm = ({ id, plan, name, chatBot }: Props) => {
     loading,
   } = useSettings(id);
   return (
-    <form className="flex flex-col gap-8 pb-10 -z-40" onSubmit={onUpdateSettings}>
+    <form
+      className="flex flex-col gap-8 pb-10 -z-40"
+      onSubmit={onUpdateSettings}
+    >
       <div className="flex flex-col gap-3">
         <h2 className="font-bold text-2xl">Domain Settings</h2>
         <Separator orientation="horizontal" />
         <DomainUpdate name={name} register={register} errors={errors} />
         <CodeSnippet id={id} />
+      </div>
+      <div className="flex flex-col gap-3 mt-5">
+        <div className="flex gap-4 items-center">
+          <h2 className="font-bold text-2xl">Chatbot Setting</h2>
+          <div className="flex gap-1 bg-cream rounded-full px-3 py-1 text-xs items-center font-bold">
+            <PremiumBadge />
+            Premium
+          </div>
+        </div>
+        <Separator orientation="horizontal" />
+        <div className="grid md:grid-cols-2">
+          <div className="col-span-1 flex flex-col gap-5">
+            <EditChatbotIcon
+              chatBot={chatBot}
+              register={register}
+              errors={errors}
+            />
+            <WelcomeMessage
+              message={chatBot?.welcomeMessage!}
+              register={register}
+              errors={errors}
+            />
+          </div>
+          <div className="col-span-1 relative">
+            <Image
+              src="/images/bot-ui.png"
+              className="sticky top-0"
+              alt="bot-ui"
+              width={530}
+              height={769}
+            />
+          </div>
+        </div>
       </div>
     </form>
   );
--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -1,28 +1,28 @@
-"use client";
-
-import useSideBar from "@/app/context/use-sidebar";
-import { cn } from "@/lib/utils";
-import React from "react";
-import MaxMenu from "./maximized-menu";
-import { MinMenu } from "./minimized-menu";
+'use client'
+import { cn } from '@/lib/utils'
+import React from 'react'
+import MaxMenu from './maximized-menu'
+import { MinMenu } from './minimized-menu'
+import useSideBar from '@/app/context/use-sidebar'
 
 type Props = {
   domains:
     | {
-        id: string;
-        name: string;
-        icon: string;
+        id: string
+        name: string
+        icon: string
       }[]
     | null
-    | undefined;
-};
+    | undefined
+}
 
 const Sidebar = ({ domains }: Props) => {
   const { expand, onExpand, page, onSignOut } = useSideBar();
+
   return (
     <div
       className={cn(
-        "bg-cream h-full w-[60px] fill-mode-forwards fixed md:relative",
+        "bg-cream dark:bg-neutral-950 h-full w-[60px] fill-mode-forwards fixed md:relative",
         expand == undefined && "",
         expand == true
           ? "animate-open-sidebar"
